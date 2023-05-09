3367be3c95a2aad45991cba86dbf7f480f811ec7
ahammermuller <alineh@gmail.com>
2023-05-09 14:09:11 -0700

    corrected the remove method

--- EXERCISE-LL-Remove.py
+++ EXERCISE-LL-Remove.py
@@ -103,7 +103,7 @@ class LinkedList:
     ##############################
   
     def remove(self, index):
-        if index < 0 or index > self.length:
+        if index < 0 or index >= self.length:
             return None
         if index == 0:
             return self.pop_first()
