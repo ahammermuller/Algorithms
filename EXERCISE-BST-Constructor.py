c024ac84e50f1de06a2afbe0f5d50b5f229a8f07
ahammermuller <alineh@gmail.com>
2023-07-31 11:32:38 -0700

    binary search trees

---
+++ EXERCISE-BST-Constructor.py
@@ -0,0 +1,38 @@
+# class Node:
+    ## WRITE NODE CONSTRUCTOR HERE ##
+    #                               #
+    #                               #
+    #                               #
+    #                               #
+    #################################
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None            
+
+# class BinarySearchTree:
+    ## WRITE BST CONSTRUCTOR HERE ##
+    #                              #
+    #                              #
+    #                              #
+    #                              #
+    ################################
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+
+my_tree = BinarySearchTree()
+
+print(my_tree.root)
+
+
+ 
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    None
+
+"""
